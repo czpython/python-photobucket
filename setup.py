c6d2107de7982496d5fe765aa5d138ad14dbdc3a
Paulo Alvarado <commonzenpython@gmail.com>
2012-07-10 04:18:46 -0400

    Updated version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="python-photobucket",
-    version="0.1",
+    version="0.2",
     description="A python wrapper for the Photobucket API",
     author="Paulo Alvarado",
     author_email="commonzenpython@gmail.com",
